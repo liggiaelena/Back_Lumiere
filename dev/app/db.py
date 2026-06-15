da966b3d17273436ccff61ec237465ecc1178ee6
Chao-Chung Liu <caatat741213@gmail.com>
2026-06-15 15:32:14 -0400

    Update db.py

--- dev/app/db.py
+++ dev/app/db.py
@@ -6,5 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("Error: DATABASE_URL not found. Please check if this variable is set in the .env file.")
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine)
